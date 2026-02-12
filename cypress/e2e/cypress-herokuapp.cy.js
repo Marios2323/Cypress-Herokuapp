54edc4b5d2403fba9dfa5e5244cc3a35b2996024
Michael <michaelgalliko@gmail.com>
2026-02-12 15:29:55 +0200

    feat: implement login tests using POM and fixture-based test data

--- cypress/e2e/cypress-herokuapp.cy.js
+++ cypress/e2e/cypress-herokuapp.cy.js
@@ -1,15 +1,36 @@
+
 import LoginPage from '../pages/LoginPage';
+import users from '../fixtures/users.json';
 
 describe('Heroku Login Test', () => {
 
   const loginPage = new LoginPage();
 
-  it('visits the Herokuapp and successfully logs in', () => {
-
+  beforeEach(() => {
     loginPage.visit();
-    loginPage.login('tomsmith', 'SuperSecretPassword!');
-
-    cy.contains('You logged into a secure area!');
   });
 
-});
+  it('Should login successfully with valid credentials', () => {
+    const validUser = users.users.find(user => user.type === 'valid');
+
+    loginPage.login(validUser.username, validUser.password);
+
+    cy.contains('#flash', 'You logged into a secure area!').should('be.visible');
+  });
+
+  it('Should display error message when username is invalid', () => {
+    const invalidUser = users.users.find(user => user.type === 'invalidUsername');
+
+    loginPage.login(invalidUser.username, invalidUser.password);
+
+    cy.contains('#flash', 'Your username is invalid!').should('be.visible');
+  });
+
+  it('Should display error message when password is invalid', () => {
+    const invalidUser = users.users.find(user => user.type === 'invalidPassword');
+      
+    loginPage.login(invalidUser.username, invalidUser.password);
+
+    cy.contains('#flash', 'Your password is invalid!').should('be.visible');
+    });
+  });
