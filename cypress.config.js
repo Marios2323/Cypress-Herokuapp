9226ad01a8bb67c546ba7d51f7e63b95097570b6
Michael <michaelgalliko@gmail.com>
2026-03-03 07:30:31 +0200

    feat(config): configure baseUrl for e2e tests

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,8 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    baseUrl: 'https://the-internet.herokuapp.com',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
