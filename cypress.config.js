bb7d65c073d8fc7deb98f3d8677a7972d6e31fe7
Michael <michaelgalliko@gmail.com>
2026-03-10 08:35:07 +0200

    feat(config): add more configurations

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,14 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "https://the-internet.herokuapp.com",
+    specPattern: "cypress/e2e/**/*.cy.{js,ts}",
     retries: {
       runMode: 2,
       openMode: 0
-    }
+    },
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: false,
+    screenshotOnRunFailure: true,
   }
 });
