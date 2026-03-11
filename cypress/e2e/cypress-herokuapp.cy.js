156dd2bd97a333ee857fcd9070d71cc67188bed5
Michael <michaelgalliko@gmail.com>
2026-03-11 09:25:59 +0200

    test(login): pass flashMessage to assertFlashMessage calls

--- cypress/e2e/cypress-herokuapp.cy.js
+++ cypress/e2e/cypress-herokuapp.cy.js
@@ -11,13 +11,14 @@ describe('Login Tests', () => {
 
   it("Valid login", () => {
     loginPage.login(Cypress.env('username'), Cypress.env('password'));
-    loginPage.assertFlashMessage(Cypress.env('expectedMessage'));
+    loginPage.assertFlashMessage(Cypress.env('expectedMessage')
+      , Cypress.env('flashClass'));
     loginPage.assertSuccessfulLogin();
   })
   users.forEach(user => {
     it(user.name, () => {
       loginPage.login(user.username, user.password);
-      loginPage.assertFlashMessage(user.expectedMessage);
+      loginPage.assertFlashMessage(user.expectedMessage, user.flashClass);
 
       if (user.loginSuccessful) {
         loginPage.assertSuccessfulLogin();
