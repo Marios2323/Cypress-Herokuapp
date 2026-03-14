28037988a33a5be0561795f5584943b0bacf0bf8
Michael <michaelgalliko@gmail.com>
2026-03-14 07:47:53 +0200

    refactor(login): nest describe blocks under parent to share beforeEach

--- cypress/e2e/cypress-herokuapp.cy.js
+++ cypress/e2e/cypress-herokuapp.cy.js
@@ -7,23 +7,24 @@ describe('Setting up...', () => {
     loginPage.elements.username().should('be.visible');
     loginPage.elements.password().should('be.visible');
   });
-});
 
-describe('Happy path', () => {
-  it("Valid login", () => {
-    cy.env(['username', 'password', 'expectedMessage', 'flashClass']).then((env) => {
-      loginPage.login(env.username, env.password);
-      loginPage.assertFlashMessage(env.expectedMessage, env.flashClass);
-      loginPage.assertSuccessfulLogin();
-    });
+
+  describe('Happy path', () => {
+    it("Valid login", () => {
+      cy.env(['username', 'password', 'expectedMessage', 'flashClass']).then((env) => {
+        loginPage.login(env.username, env.password);
+        loginPage.assertFlashMessage(env.expectedMessage, env.flashClass);
+        loginPage.assertSuccessfulLogin();
+      });
+    })
+  });
+
+  describe("Negative login tests", () => {
+    users.forEach(user => {
+      it(user.name, () => {
+        loginPage.login(user.username, user.password);
+        loginPage.assertFlashMessage(user.expectedMessage, user.flashClass);
+      });
+    })
   })
 });
-
-describe("Negative login tests", () => {
-  users.forEach(user => {
-    it(user.name, () => {
-      loginPage.login(user.username, user.password);
-      loginPage.assertFlashMessage(user.expectedMessage, user.flashClass);
-    });
-  })
-})
